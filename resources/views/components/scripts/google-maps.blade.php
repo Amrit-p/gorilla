7434b5e03598e7f6e1fe84f1730b0b0e28e3e3e0
amrit <amritpal6239@gmail.com>
2026-05-31 22:38:07 +0530

    fixed google map link

--- resources/views/components/scripts/google-maps.blade.php
+++ resources/views/components/scripts/google-maps.blade.php
@@ -1,6 +1,8 @@
 @php
     $mapsLoaderVersion = @filemtime(public_path('js/google-maps-loader.js')) ?: 1;
+    $mapsMarkersVersion = @filemtime(public_path('js/google-maps-markers.js')) ?: 1;
     $addressPickerVersion = @filemtime(public_path('js/google-address-picker.js')) ?: 1;
 @endphp
 <script src="{{ asset('js/google-maps-loader.js') }}?v={{ $mapsLoaderVersion }}"></script>
+<script src="{{ asset('js/google-maps-markers.js') }}?v={{ $mapsMarkersVersion }}"></script>
 <script src="{{ asset('js/google-address-picker.js') }}?v={{ $addressPickerVersion }}"></script>
